a1904a8f0de0b04ce2a9350de6c6f7c7ffae489f
Grant Royseth <groyseth@gmail.com>
2022-05-14 22:16:13 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
-
+import challenge from './image/GrantRoyseth.svg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        
-        </a>
-        {/* https://blog.logrocket.com/how-to-use-svgs-in-react/ */}
-        
-      </header>
+      <img src={challenge} />
     </div>
   );
 }
